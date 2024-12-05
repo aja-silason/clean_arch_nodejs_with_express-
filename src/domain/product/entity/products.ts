e9ffd3b4e2c0e00cf333d858a0235104daa3116c
Anania Augusto <ananiasjaimeaugusto@gmail.com>
2024-12-05 20:59:39 +0100

    fix(product-domain-entity): fix uuid form use

--- src/domain/product/entity/products.ts
+++ src/domain/product/entity/products.ts
@@ -11,7 +11,7 @@ export class Product {
 
     public static create(name: string, price: number){
         return new Product({
-            id: crypto.randomUUID.toString(),
+            id: crypto.randomUUID().toString(),
             name,
             price,
             quandity: 0
